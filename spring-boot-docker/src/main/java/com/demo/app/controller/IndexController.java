5be3675fa8dc647d3026669ef3b2da149b4d4cd9
julicastro <castroejulian@gmail.com>
2024-01-01 20:43:30 -0300

    commit

---
+++ spring-boot-docker/src/main/java/com/demo/app/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.demo.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class IndexController {
+
+    @GetMapping("test")
+        public String welcome(){
+        return "this endpoint is working";
+    }
+}
