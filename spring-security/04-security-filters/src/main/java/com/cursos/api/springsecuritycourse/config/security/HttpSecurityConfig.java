85e6a40b8cac58a07d184dc9fb94cbd54ed9e9e4
julicastro <castroejulian@gmail.com>
2024-01-21 22:17:49 -0300

    commit

---
+++ spring-security/04-security-filters/src/main/java/com/cursos/api/springsecuritycourse/config/security/HttpSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.cursos.api.springsecuritycourse.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class HttpSecurityConfig {
+
+    @Autowired
+    private AuthenticationProvider daoAuthProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        SecurityFilterChain filterChain = http
+                .csrf(csrfConfig -> csrfConfig.disable()) // proteccion contra csrf
+                .sessionManagement(sessMagConfig -> sessMagConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // tipo de app con o sin estado
+                .authenticationProvider(daoAuthProvider) // estrategia
+                .authorizeHttpRequests(authReqConfig -> {
+                    authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
+                    authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate-token").permitAll();
+                    authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
+                    // tenemos q decirle q endpoints necesitan login
+                    authReqConfig.anyRequest().authenticated(); // cualquier request q no sean las anteriores, necesitan del login.
+                })
+                .build(); // crea un DefaultSecurityFilterChain
+        return filterChain;
+    }
+
+
+
+
+}
---
+++ spring-security/04-security-filters/src/main/java/com/cursos/api/springsecuritycourse/service/impl/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.cursos.api.springsecuritycourse.service.impl;
+
+import com.cursos.api.springsecuritycourse.dto.SaveUser;
+import com.cursos.api.springsecuritycourse.exception.InvalidPasswordException;
+import com.cursos.api.springsecuritycourse.persistence.entity.User;
+import com.cursos.api.springsecuritycourse.persistence.repository.UserRepository;
+import com.cursos.api.springsecuritycourse.persistence.util.Role;
+import com.cursos.api.springsecuritycourse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    /* inyecta nuestro bean del SecurityBeanInjector, el cual devuelve un BCryptPasswordEncoder*/
+
+    @Override
+    public User registerOneCustomer(SaveUser newUser) {
+        validatePassword(newUser);
+        User user = new User();
+        user.setName(newUser.getName());
+        user.setPassword(passwordEncoder.encode(newUser.getPassword()));
+        user.setRole(Role.ROLE_CUSTOMER);
+        user.setUsername(newUser.getUsername());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findOneByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    private void validatePassword(SaveUser dto) {
+        if (!StringUtils.hasText(dto.getPassword()) || !StringUtils.hasText(dto.getRepeatedPassword())) {
+            throw new InvalidPasswordException("Passwords don't match");
+        }
+        if (!dto.getPassword().equals(dto.getRepeatedPassword())) {
+            throw new InvalidPasswordException("Passwords don't match");
+        }
+    }
+
+
+
+
+}
