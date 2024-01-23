51a7f7f41359c3d2ce102918ba1b404c9ba3df6e
julicastro <castroejulian@gmail.com>
2024-01-23 03:27:23 -0300

    commit

---
+++ spring-security/04-authorization-architecture/src/main/java/com/cursos/api/springsecuritycourse/persistence/util/RolePermission.java
@@ -0,0 +1,23 @@
+package com.cursos.api.springsecuritycourse.persistence.util;
+
+public enum RolePermission {
+
+    // product permission
+    READ_ALL_PRODUCTS,
+    READ_ONE_PRODUCT,
+    CREATE_ONE_PRODUCT,
+    UPDATED_ONE_PRODUCT,
+    DISABLED_ONE_PRODUCT,
+
+    // category permission
+    READ_ALL_CATEGORIES,
+    READ_ONE_CATEGORY,
+    CREATE_ONE_CATEGORY,
+    UPDATED_ONE_PCATEGORY,
+    DISABLED_ONE_CATEGORY,
+
+    // user permission
+    READ_MY_PROFILE
+
+
+}
