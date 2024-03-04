0d1f5ccdd6704dc1a3c3628f1f955297200cde11
julicastro <castroejulian@gmail.com>
2024-03-04 19:56:05 -0300

    commit

---
+++ spring-security/04-authorization-architecture-role-fixed-permissions-strategy/src/main/java/com/cursos/api/springsecuritycourse/config/security/HttpSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.cursos.api.springsecuritycourse.config.security;
+
+import com.cursos.api.springsecuritycourse.config.security.filter.JwtAuthenticationFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class HttpSecurityConfig {
+
+    @Autowired
+    private AuthenticationProvider daoAuthProvider;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        SecurityFilterChain filterChain = http
+                .csrf( csrfConfig -> csrfConfig.disable() )
+                .sessionManagement( sessMagConfig -> sessMagConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+                .authenticationProvider(daoAuthProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests( authReqConfig -> {
+
+                    authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
+                    authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
+                    authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate-token").permitAll();
+
+                    authReqConfig.anyRequest().authenticated();
+                } )
+                .build();
+
+        return filterChain;
+    }
+
+}
