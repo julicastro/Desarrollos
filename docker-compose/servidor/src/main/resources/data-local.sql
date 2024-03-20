fcf8c475f28e9955e9a341aabc2a02a782a6f7bf
julicastro <castroejulian@gmail.com>
2024-03-20 13:57:40 -0300

    commit

--- docker-compose/servidor/src/main/resources/data-local.sql
+++ docker-compose/servidor/src/main/resources/data-local.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO prueba (name, email, phone) VALUES ('local1', 'local1@mail.com', '12345678');
+INSERT INTO client (name, email, phone) VALUES ('local1', 'local1@mail.com', '12345678');
 INSERT INTO client (name, email, phone) VALUES ('local2', 'local2@mail.com', '78945612');
 INSERT INTO client (name, email, phone) VALUES ('local3', 'local3@mail.com', '12345655');
 INSERT INTO client (name, email, phone) VALUES ('local4', 'local4@mail.com', '32345444');
