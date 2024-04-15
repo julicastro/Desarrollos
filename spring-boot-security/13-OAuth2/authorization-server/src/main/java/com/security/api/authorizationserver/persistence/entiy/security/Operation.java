9a8c716170fc7d0070b1036f1e0d2a90629e1ac9
julicastro <castroejulian@gmail.com>
2024-04-15 19:38:36 -0300

    commit

---
+++ spring-boot-security/13-OAuth2/authorization-server/src/main/java/com/security/api/authorizationserver/persistence/entiy/security/Operation.java
@@ -0,0 +1,70 @@
+package com.security.api.authorizationserver.persistence.entiy.security;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Operation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String path;
+
+    private String httpMethod;
+
+    private boolean permitAll;
+
+    @Column(name = "module_id")
+    private Long moduleId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public void setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public boolean isPermitAll() {
+        return permitAll;
+    }
+
+    public void setPermitAll(boolean permitAll) {
+        this.permitAll = permitAll;
+    }
+
+    public Long getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(Long moduleId) {
+        this.moduleId = moduleId;
+    }
+}
