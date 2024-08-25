d9b15f89a696e2fd8772ebc0c04d35a1d6651061
julicastro <castroejulian@gmail.com>
2024-08-25 18:28:29 -0300

    commit

---
+++ Algoritmos, Estructuras de Datos y Entrevistas Programación/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
@@ -0,0 +1,28 @@
+package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_unique;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Dado un método que recibe una String, comprobar si todos los caracteres son únicos o no.
+ *
+ * isUnique("abcde") => true;
+ * isUnique("abcded") => false;
+ */
+public class IsUnique {
+
+  public boolean isUnique(String s) {
+    Map<Integer, Character> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+
+    }
+
+
+
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+
+
+
+}
