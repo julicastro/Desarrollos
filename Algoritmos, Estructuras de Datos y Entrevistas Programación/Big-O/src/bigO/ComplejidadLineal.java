12fa62703469f317f60203432ade2ca1afd454a7
julicastro <castroejulian@gmail.com>
2024-08-25 01:18:04 -0300

    commit

---
+++ Algoritmos, Estructuras de Datos y Entrevistas Programación/Big-O/src/bigO/ComplejidadLineal.java
@@ -0,0 +1,28 @@
+package bigO;
+
+public class ComplejidadLineal {
+
+    /*
+    O(N) -> Complejidad Lineal.
+    El tiempo de ejecución del algoritmo aumenta linealmente con respecto al tamaño de la entrada.
+     */
+
+    public static void main(String[] args) {
+        int [] array = {15, 44, 23, 12};
+        reverseArray(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+
+    public static void reverseArray (int [] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int reversePosition = array.length - i - 1;
+            int tempValue = array[reversePosition];
+            array[reversePosition] = array[i];
+            array[i] = tempValue;
+        }
+    }
+
+
+}
